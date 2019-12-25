7ab11a055f6f201711977989357efa3bbf3cf3bf
Sangoon_Is_Noob <cotwo0139@gmail.com>
2019-12-25 15:31:03 +0900

    Updated Informations

--- examples/test.js
+++ examples/test.js
@@ -1,10 +1,11 @@
 const Lyrics = require('../lib/index')
 const lyrics = new Lyrics()
 
-async function test () {
+async function test (provider, title) {
   const start = new Date().getTime()
-  const result = await lyrics.get('melon', 'Takeaway')
+  const result = await lyrics.get(provider, title)
   console.log(result)
+  console.log(result.result)
   console.log(new Date().getTime() - start + 'ms')
 }
-test()
+test('melon', 'Takeaway')
